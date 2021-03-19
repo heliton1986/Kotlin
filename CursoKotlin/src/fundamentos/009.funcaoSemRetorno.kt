94dfb0a9e2fabe9bd55c3cc318741ba9b6034ed0
Heliton Ribeiro Ferreira <heliton.ds86@gmail.com>
2021-03-19 20:04:33 -0300

    Adicionando mais arquivos a fundamentos

---
+++ CursoKotlin/src/fundamentos/009.funcaoSemRetorno.kt
@@ -0,0 +1,11 @@
+package fundamentos
+
+// função sem retorno
+fun imprimirSoma(a: Int, b: Int) {
+    println(a + b)
+}
+
+// imprimindo a função sem retorno, apenasinsira a funçao e seus valores
+fun main() {
+    imprimirSoma(4, 5)
+}
