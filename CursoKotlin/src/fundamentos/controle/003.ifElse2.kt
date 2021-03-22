ac868a07ca0e1aa55dceb26ae6f500de8f5a82d2
Heliton Ribeiro Ferreira <heliton.ds86@gmail.com>
2021-03-22 16:31:32 -0300

    Adicionando arquivo 004.ifelse3.kt

---
+++ CursoKotlin/src/fundamentos/controle/003.ifElse2.kt
@@ -0,0 +1,16 @@
+package fundamentos.controle
+
+fun main() {
+    val num1: Int = 5
+    val num2: Int = 3
+
+    val maiorValor = if (num1 > num2) {
+        println("processando if...")
+        num1
+    } else {
+        println("processando else...")
+        num2
+    }
+
+    println("O maior valor é ${maiorValor}.")
+}
---
+++ CursoKotlin/src/fundamentos/controle/004.ifElse3.kt
@@ -0,0 +1,11 @@
+package fundamentos.controle
+
+fun main() {
+    val num1: Int = 5
+    val num2: Int = 3
+
+    val maiorValor = if (num1 > num2) num1 else num2
+
+
+    println("O maior valor é ${maiorValor}.")
+}
