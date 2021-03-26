2a69cba573feed4240aabd0200a216345800a0e9
Heliton Ribeiro Ferreira <heliton.ds86@gmail.com>
2021-03-26 15:51:11 -0300

    Add TesteMesa.kt

--- CursoKotlin/src/fundamentos/controle/003.ifElse2.kt
+++ CursoKotlin/src/fundamentos/controle/003.ifElse2.kt
@@ -1,7 +1,7 @@
 package fundamentos.controle
 
 fun main() {
-    val num1: Int = 5
+    val num1: Int = 2
     val num2: Int = 3
 
     val maiorValor = if (num1 > num2) {
--- CursoKotlin/src/fundamentos/controle/TestaMes.kt
+++ CursoKotlin/src/fundamentos/controle/TestaMes.kt
@@ -1,2 +1,22 @@
 package fundamentos.controle
 
+fun main() {
+
+    val mes: Int = 0;
+
+    when(mes) {
+        1 -> println("Janeiro")
+        2 -> println("Fevereiro")
+        3 -> println("Março")
+        4 -> println("Abril")
+        5 -> println("Maio")
+        6 -> println("Junho")
+        7 -> println("Julho")
+        8 -> println("Agosto")
+        9 -> println("Setembro")
+        10 -> println("Outubro")
+        11 -> println("Novembro")
+        12 -> println("Dezembro")
+        else -> println("Fim")
+    }
+}
